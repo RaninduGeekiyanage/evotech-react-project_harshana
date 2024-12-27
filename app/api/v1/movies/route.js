f6f7d521697611b2c8792b4af10023819357c050
hashaaan <shalithadev@gmail.com>
2024-12-27 19:59:48 +0530

    21/12/2024 Session

--- app/api/v1/movies/route.js
+++ app/api/v1/movies/route.js
@@ -1,16 +1,28 @@
 import { NextResponse } from "next/server";
-
-const MOVIES = [
-  { id: 1, title: "Harry Potter 1" },
-  { id: 2, title: "Load of the Rings" },
-  { id: 3, title: "Hobbit" },
-  { id: 4, title: "Insteller" },
-  { id: 5, title: "Harry Potter 2" },
-  { id: 6, title: "Harry Potter 3" },
-  { id: 7, title: "Harry Potter 4" },
-  { id: 8, title: "Harry Potter 5" },
-];
+import clientPromise from "@/app/libs/mongodb";
 
 export const GET = async (req) => {
-  return NextResponse.json({ success: true, movies: MOVIES });
+  // Get Movies from the MongoDB
+  try {
+    const client = await clientPromise();
+
+    // sample_mflix is the database name
+    const db = client.db("sample_mflix");
+
+    // fetch movies from the database
+    const movies = await db
+      .collection("movies")
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(10)
+      .toArray();
+
+    return NextResponse.json(movies);
+  } catch (error) {
+    console.log("MONGODB ERROR", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 };
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,13 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { getMovies } from "../libs/apis/server";
+import Image from "next/image";
 
 export default async function DashboardPage() {
   // 1. Add shadcn Card
@@ -6,7 +15,9 @@ export default async function DashboardPage() {
   // 3. Read the dummy response
   // 4. Render data set in the UI
 
-  const { movies } = await getMovies();
+  const moviesQuery = await getMovies();
+
+  console.log("MOVIES FE", moviesQuery);
 
   return (
     <main>
@@ -20,10 +31,31 @@ export default async function DashboardPage() {
       {/* body section */}
       <div className="container mt-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {movies?.length &&
-            movies.map((movie) => (
-              <div key={movie.id} className="h-96 bg-green-400">
-                {movie?.title}
+          {moviesQuery?.length &&
+            moviesQuery.map((movie) => (
+              <div key={movie?._id} className="h-[480px]">
+                <Card className="h-full">
+                  <CardHeader>
+                    <CardTitle>{movie?.title}</CardTitle>
+                    <CardDescription className="sr-only">
+                      {movie?.title}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex justify-center bg-black w-full h-[220px] mb-4 rounded">
+                      <Image
+                        src={movie?.poster}
+                        alt={movie?.title}
+                        width={200}
+                        height={400}
+                        className="h-full w-auto object-contain"
+                        priority={true}
+                      />
+                    </div>
+                    <p className="line-clamp-3">{movie?.plot}</p>
+                  </CardContent>
+                  <CardFooter className="flex justify-between"></CardFooter>
+                </Card>
               </div>
             ))}
         </div>
